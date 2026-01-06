b9c01b325658def83e8a9a74e599bd8f7a37c10e
Cylerss <mmuralikrishna2007@gmail.com>
2026-01-06 21:47:20 +0530

    Jwt Auth Added

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI,HTTPException,File,UploadFile,Form,Depends
-from src.db import create_db_and_tables,get_session,Post
+from .db.db import create_db_and_tables,get_session,Post
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy.future import select
-from src.images import imagekit
+from .images import imagekit
 import uuid
+from .auth.users import current_active_user,auth_backend,fastapi_users
+from .models.schemas import UserRead,PostResponse,PostCreate,UserCreate,UserUpdate
 
 
 @asynccontextmanager
@@ -13,7 +15,12 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-
+app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])
+app.include_router(fastapi_users.get_register_router(UserRead,UserCreate), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(UserRead), prefix="/auth", tags=["auth"])   
+app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/users", tags=["users"])
+                                                             
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
--- src/db/db.py
+++ src/db/db.py
@@ -1,6 +1,5 @@
 from collections.abc import AsyncGenerator
-import datetime
-from datetime import timezone
+from datetime import timezone,datetime
 import uuid
 from dotenv import load_dotenv
 from sqlalchemy import Column, String, Text,DateTime,ForeignKey
@@ -8,10 +7,18 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine,async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 import os
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
+from fastapi import Depends
 
-load_dotenv()
+
+load_dotenv(os.path.join(os.path.dirname(__file__), "../.env"))
+
 class Base(DeclarativeBase):
     pass
+
+class User(SQLAlchemyBaseUserTableUUID,Base):
+    posts=relationship("Post",back_populates="user")
+
 
 DATABASE_URL=os.getenv("POSTGES_URL")
 
@@ -19,11 +26,13 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id=Column(UUID(as_uuid=True), ForeignKey("user.id"),nullable=False)
+    user=relationship("User",back_populates="posts")
     caption=Column(Text)
     url=Column(String, nullable=False)
     file_type=Column(String, nullable=False)
     file_name=Column(String, nullable=False)
-    created_at=Column(DateTime, nullable=False,default=lambda: datetime.datetime.now(timezone.utc).replace(tzinfo=None))
+    created_at=Column(DateTime, nullable=False,default=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
 
 engine = create_async_engine(DATABASE_URL, echo=True, connect_args={'ssl': 'require'})
 async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
@@ -31,7 +40,14 @@ async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncS
 async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-        
+
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
+        
+        
+async def get_user_db(session: AsyncSession = Depends(get_session)):
+    yield SQLAlchemyUserDatabase(session, User)
+    
+
+    
